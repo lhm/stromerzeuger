1e06fa3cb75a76df71e26e23df2ac19e4b6a2316
Lars Henrik Mai <lars.mai@kontinui.de>
2020-05-13 09:13:00 +0200

    Add data to geojson

---
+++ scripts/geo.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import geopandas as gpd
+from utils import root
+
+df = pd.read_csv(root / "data" / "nettonennleistung.csv", dtype={"AGS": "object"})
+
+gdf = gpd.read_file(root / "files" / "gemeinden-sn.json")
+gdf = gdf[["id", "GEN", "BEZ", "AGS_0", "geometry"]]
+gdf.columns = ["id", "Name", "Bezeichnung", "AGS", "geometry"]
+
+result = gdf.merge(df, on="AGS", how="left")
+result.to_file(root / "data" / "nettonennleistung-sn.json", driver="GeoJSON")
