b17729ae13a3d60c248529791d3c64a22047e4d7
Lars Henrik Mai <lars.mai@kontinui.de>
2020-05-16 12:17:16 +0200

    Convert all columns of excel during data prep

--- scripts/convert.py
+++ scripts/convert.py
@@ -1,22 +1,49 @@
 import pandas as pd
-from utils import root
+import xlrd
+from utils import filesdir, datadir, log
 
-cols = {
-    "ENH_MastrNummer": "string",
-    "ENH_Betriebsstatus": "category",
-    "ENH_EinheitenTyp": "string",
-    "ENH_InbetriebnahmeDatum": "datetime64[ns]",
-    "ENH_LetzteAktualisierung": "datetime64[ns]",
-    "ENH_Gemeindeschluessel": "string",
-    "ENH_Bruttoleistung": "float",
-    "ENH_Nettonennleistung": "float",
-}
+sheet_names = [
+    "Tabelle_IDX",
+    "Tabelle_ENH",
+    "Tabelle_EEG",
+    "Tabelle_KWK",
+    "Tabelle_SGE",
+    "Tabelle_MAK",
+]
 
-df = pd.read_excel(
-    root / "files" / "200303_MaStR-Daten_registriert_ab_31-01-2019.xlsx",
-    sheet_name="Tabelle_ENH",
-    usecols=cols.keys(),
-    dtype=cols,
-)
+sourcefile = filesdir / "200303_MaStR-Daten_registriert_ab_31-01-2019.xlsx"
 
-df.to_parquet(root / "data" / "raw.parquet")
+output_dir = datadir / sourcefile.stem / "raw"
+output_dir.mkdir(exist_ok=True, parents=True)
+
+
+def read_sheet(sheet_name, dtypes=None):
+    log.msg("Reading sheet", sheet_name=sheet_name, sourcefile=sourcefile)
+    workbook = xlrd.open_workbook(sourcefile, on_demand=True)
+    with pd.ExcelFile(workbook) as xls:
+        df = pd.read_excel(xls, sheet_name=sheet_name, dtype=dtypes)
+    return df
+
+
+def write_schema(df, sheet_name):
+    output_path = output_dir / f"{sheet_name}-schema.csv"
+    log.msg("Writing schema", sheet_name=sheet_name, output_path=output_path)
+    df.dtypes.to_csv(output_path, header=False)
+
+
+def write_sheet(df, sheet_name):
+    output_path = output_dir / f"{sheet_name}.parquet"
+    log.msg("Writing sheet", sheet_name=sheet_name, output_path=output_path)
+    df.to_parquet(output_path)
+
+
+def main():
+    sheet_name = "Tabelle_ENH"
+    dtypes = {"ENH_Gemeindeschluessel": "string"}
+
+    df = read_sheet(sheet_name, dtypes=dtypes)
+    write_schema(df, sheet_name)
+    write_sheet(df, sheet_name)
+
+
+main()
